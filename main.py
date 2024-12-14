e6358c89438dbf657fd9b9616b775c6babd21108
hrcyber <cyber.zone.nk@gmail.com>
2024-12-14 20:09:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CZ",
+    page_icon=":pizza:",
+)
+st.title("Pizza Delivery App Is In Progress!")
+st.sidebar.success("select a Page Above")
