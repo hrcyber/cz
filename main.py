67d4caad7dd69165db1965ebc6de65cea539dbba
hrcyber <cyber.zone.nk@gmail.com>
2024-12-14 20:19:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="CZ",
-    page_icon=":pizza:",
-)
+    page_title="CZ")
 st.title("Pizza Delivery App Is In Progress!")
-st.sidebar.success("select a Page Above")
